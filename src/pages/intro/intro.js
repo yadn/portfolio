120a6d1b8f8eaa644b1fb5a3dede4e8e362ef70b
Yadnyesh Patil <ypatil@enphaseenergy.com>
2025-12-03 22:39:31 +0530

    update typewriter words

--- src/pages/intro/intro.js
+++ src/pages/intro/intro.js
@@ -5,14 +5,14 @@ import useTypewriter from '../../hooks/useTypeWriter';
 
 const Intro = () => {
 
-  const words = ["A passionate individual", "Curious and fast learner", "Full Stack Developer", "Collaborative team contributor", "Optimistic and solution-oriented"];
+  const words = ["Java", "Spring Boot", "React JS", "JavaScript", "HTML & CSS", "Docker", "MongoDB", "Ruby On Rails", "AWS"];
   const text = useTypewriter(words, 150, 50, 1500);
 
   return (<>
     <div className='textbox'>
       <div className='name'>Yadnyesh</div>
       <div className='description'>
-        {"> " + text}
+        {"> Has Hands-On experience in " + text}
         <span className="cursor"></span>
       </div>
     </div>
